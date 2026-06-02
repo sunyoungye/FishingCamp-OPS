a46b78615b40ac5c758f6f37245299eb6996c88d
sunyoungye <101558774+sunyoungye@users.noreply.github.com>
2026-06-02 18:45:02 +0900

    File Setting

---
+++ Assets/Scripts/Player/PlayerCtrl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public float movSpeed;
+    float speedX, speedY;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * movSpeed;
+        rb.velocity = new Vector2(speedX, speedY);
+    }
+}
